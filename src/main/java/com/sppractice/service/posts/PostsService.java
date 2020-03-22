fed8ff6dc6415e6c027e1611a8d40a45490af5d1
flamme <flamme1004@gmail.com>
2020-03-22 21:14:16 +0900

    2020/03/22/ JPA실험

---
+++ src/main/java/com/sppractice/service/posts/PostsService.java
@@ -0,0 +1,37 @@
+package com.sppractice.service.posts;
+
+import com.sppractice.domain.posts.Posts;
+import com.sppractice.domain.posts.PostsRepository;
+import com.sppractice.web.dto.PostsResponseDto;
+import com.sppractice.web.dto.PostsSaveRequestDto;
+import com.sppractice.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
