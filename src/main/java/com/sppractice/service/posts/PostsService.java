82e4d4bbab7c13081b8e17ca2d58d8f172073e63
KHJ <flamme1004@kaoni.com>
2020-03-23 20:34:32 +0900

    OAUTH2 적용
    현재 : 실행시 ERROR BEAN을 찾을 수 없다고함.

--- src/main/java/com/sppractice/service/posts/PostsService.java
+++ src/main/java/com/sppractice/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.sppractice.service.posts;
 
 import com.sppractice.domain.posts.Posts;
 import com.sppractice.domain.posts.PostsRepository;
+import com.sppractice.web.dto.PostsListResponseDto;
 import com.sppractice.web.dto.PostsResponseDto;
 import com.sppractice.web.dto.PostsSaveRequestDto;
 import com.sppractice.web.dto.PostsUpdateRequestDto;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -27,6 +30,22 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+    /* readOnly = true로 줄시 트랜잭션 범위는 유지하되, 조회 기능만 남겨두어 조회 속도가 개선된다.
+        .map(PostsListReponseDto::new -> map(Posts -> new PostsListResponseDto(posts))와 같음.
+        postsRepository 결과로 넘오온 Posts의 Stream을 map을 통해 postsListReponseDto 변환 -> lists로 반환하는 메소드
+    */
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        postsRepository.delete(posts);
     }
 
     public PostsResponseDto findById (Long id) {
---
+++ src/main/java/com/sppractice/web/IndexController.java
@@ -0,0 +1,57 @@
+package com.sppractice.web;
+
+import com.sppractice.config.auth.dto.SessionUser;
+import com.sppractice.service.posts.PostsService;
+import com.sppractice.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+    private final HttpSession httpSession;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+
+
+
+}
